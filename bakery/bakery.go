acb408ca1c535beef2ff6870a0eee6db6737ccdb
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2024-08-16 10:19:00 +0400

    reformat files

--- bakery/bakery.go
+++ bakery/bakery.go
@@ -1,5 +1,14 @@
 package bakery
 
+import (
+	"fmt"
+	"math/rand/v2"
+)
+
+type SharedMemory struct {
+	MagicNum int
+}
+
 type Bakery struct {
 	Number   []int
 	Choosing []bool
@@ -19,4 +28,81 @@ func (bakery *Bakery) FindMax() (max int) {
 		}
 	}
 	return
+}
+
+func (bakeryState *Bakery) BakeryAlgorithm(sharedMemory *SharedMemory, pid int) {
+	// Run entry code
+	num := entry(pid)
+
+	// Choosing Phase
+	bakeryState.Choosing[pid] = true
+	bakeryState.Number[pid] = bakeryState.FindMax() + 1
+	bakeryState.Choosing[pid] = false
+
+	// Waiting Phase
+	for i := 0; i < len(bakeryState.Number); i++ {
+		if i == pid {
+			continue
+		}
+		// Wait until the other process is done choosing
+		for bakeryState.Choosing[i] {
+			// Leave empty to force a busy wait
+		}
+		// Wait until the other process has a higher number or the same number but higher PID
+		for bakeryState.Number[i] != 0 && (bakeryState.Number[i] < bakeryState.Number[pid] || (bakeryState.Number[i] == bakeryState.Number[pid] && i < pid)) {
+			// Leave empty to force a busy wait
+		}
+	}
+
+	// Enter critical section
+	magicNum := criticalSection(sharedMemory, pid, num)
+	// Run exit code
+	exit(magicNum, pid)
+	// Hand over lock
+	bakeryState.Number[pid] = 0
+	// Run rest of code
+	remainder(pid)
+}
+
+// Preparation for entering the critical section
+func entry(pid int) (num int) {
+	fmt.Printf("Process %v entered entry portion\n", pid)
+	numOperations := rand.IntN(100)
+	for i := 0; i < numOperations; i++ {
+		num += rand.IntN(100)
+	}
+	fmt.Printf("Process %v leaving entry portion\n", pid)
+	return
+}
+
+// Executed immediately after critical section (before lock is given up)
+func exit(magicNum, pid int) {
+	fmt.Printf("Process %v entered exit portion\n", pid)
+	fmt.Printf("Process %v has magic number of %v\n", pid, magicNum)
+	fmt.Printf("Process %v leaving exit portion\n", pid)
+}
+
+// Simulate remainder of program
+func remainder(pid int) {
+	fmt.Printf("Process %v entered remainder portion\n", pid)
+	numOperations := rand.IntN(10000)
+	for i := 0; i < numOperations; i++ {
+	}
+	fmt.Printf("Process %v leaving remainder portion\n", pid)
+}
+
+func criticalSection(sharedMemory *SharedMemory, pid, num int) int {
+	fmt.Printf("Process %v entered critical section\n", pid)
+	switch num % 4 {
+	case 0:
+		sharedMemory.MagicNum = sharedMemory.MagicNum + num
+	case 1:
+		sharedMemory.MagicNum = sharedMemory.MagicNum - num
+	case 2:
+		sharedMemory.MagicNum = sharedMemory.MagicNum * num
+	case 3:
+		sharedMemory.MagicNum = sharedMemory.MagicNum / num
+	}
+	fmt.Printf("Process %v left critical section\n", pid)
+	return sharedMemory.MagicNum
 }
